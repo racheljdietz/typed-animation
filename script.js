9d1e44deda6c911119abfe5f59439642d425712e
racheljdietz <97128696+racheljdietz@users.noreply.github.com>
2022-03-23 16:51:13 -0400

    Update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$('input').keydown(function(e){
+$('input').keydown(function (e) {
 
     // if the space bar is clicked, move input text to the left
     if (e.keyCode == 32) {
@@ -14,27 +14,42 @@ $('input').keydown(function(e){
 })
 
 
-var checkPTags = setInterval(function(){
-    if ($("p").length > 0){ // Check if element has been found
-      // There is a <p> element in here!!
-      floatAway();
+var checkPTags = setInterval(function () {
+    if ($("p").length > 0) { // Check if <p> element has been found
+        floatAway();
     }
-  },2000);
+}, 2000); // check for <p> elements every 2 seconds
 
+
+// animation function to float text away 
 function floatAway() {
- 
-   anime({
-    targets: 'p',
-    translateX: function(){return anime.random(-300, -200);},
-    rotate:-180,
-    opacity:function(){return .5;},
-    translateY: function(){return anime.random(60, 50);},
-    scale:0,
-    skew: function () {return anime.random(9, 10);},
-    delay: anime.stagger(300, {direction: 'reverse'}),
-    easing: "easeInOutSine",
-    complete: function(anim) {
-        if(anim.completed){$('p[style*="opacity: 0.5"]').remove()};
-      }
-  });
+
+    anime({
+        targets: 'p',
+        translateX: function () { return anime.random(-300, -200); },
+        rotate: -180,
+        opacity: function () { return .5; },
+        translateY: function () { return anime.random(60, 50); },
+        scale: 0,
+        skew: function () { return anime.random(9, 10); },
+        delay: anime.stagger(300, { direction: 'reverse' }),
+        easing: "easeInOutSine",
+        complete: function (anim) {
+            if (anim.completed) { $('p[style*="opacity: 0.5"]').remove() };
+        }
+    });
 }
+
+$("#rage").click(function () {
+    $("#flames").show();
+
+    $("#peaceful").removeClass('active');
+    $("#rage").addClass('active');
+});
+
+$("#peaceful").click(function () {
+    $("#flames").hide();
+
+    $("#rage").removeClass('active');
+    $("#peaceful").addClass('active');
+});
